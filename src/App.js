89ae9a9145dcd94d21113b8707df990adc3dfdf8
joykw <wanjikujoy@rocketmail.com>
2020-02-12 22:46:27 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import items from "./items.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let lawFirm;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>LawyerApp</div>
+  
+</div>
+
+        <ul>
+          {this.state.items.map((element, index) => (<li key={index}> <div class="card" style="width: 18rem;">
+            <div class="container">
+              <div class="row">
+                <div class="col -md-3 col sm-6">
+                  <div class="card">
+                    <div class="card-block">
+                      <div class="card-title">
+                        <h4>{element.name}</h4>
+                      </div>
+                      <div class
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>{element.name}</li>
+          ))}
+        </ul>
+      
+   
+    };
 }
 
 export default App;
