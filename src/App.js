2f06d4f3c16626d508fde8bf31b0f1d1862edb88
joykw <wanjikujoy@rocketmail.com>
2020-02-13 06:06:40 +0100

    Refuses to take two default exports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import items from "./items.json";
+import { GoogleApiWrapper } from "google-maps-react";
 
-let lawFirm;
+//let lawFirm;
 
 class App extends Component {
   constructor(props) {
@@ -15,36 +16,23 @@ class App extends Component {
     return (
       <div>
         <div>LawyerApp</div>
-  
-</div>
-
         <ul>
-          {this.state.items.map((element, index) => (<li key={index}> <div class="card" style="width: 18rem;">
-            <div class="container">
-              <div class="row">
-                <div class="col -md-3 col sm-6">
-                  <div class="card">
-                    <div class="card-block">
-                      <div class="card-title">
-                        <h4>{element.name}</h4>
-                      </div>
-                      <div class
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <h5 class="card-title">Card title</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>{element.name}</li>
+          {this.state.items.map((element, index) => (
+            <li key={index}>{element.name}</li>
           ))}
         </ul>
-      
-   
-    };
+        <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}></InfoWindow>
+        </Map>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyANr7waOlZfZsBlXze1mgfbogvptrp6N5Y"
+})(App);
