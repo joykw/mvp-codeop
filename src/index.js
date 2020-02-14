d4b77176eacc9fa84c0cc2db1f303a0726155b50
joykw <wanjikujoy@rocketmail.com>
2020-02-14 08:15:15 +0100

    app progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+require("dotenv").config();
 
+console.log(process.env);
 
+ReactDOM.render(<App />, document.getElementById("root"));
